907c8e1001db0e45cbec97dc2d7d48e9d856205d
emfritz2 <emfritz2@gmail.com>
2018-02-14 20:46:55 -0600

    working homepage and pick a gamepage

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import Container from "../components/Container";
+import LoginForm from "../components/LoginForm";
+
 
 class Login extends Component {
   state = {
@@ -10,8 +12,8 @@ class Login extends Component {
   render() {
     return (
       <Container style={{ minHeight: "80%" }}>
-        <h1 className="text-center">Login page</h1>
-
+        <img src={require("../onDeckLogo.png")} />
+          <LoginForm />
       </Container>
     );
   }
