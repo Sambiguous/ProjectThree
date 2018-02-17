4f45c6b05b8958a50c4de5ebbee0ad55b25efa00
Rich T <rtranter25@gmail.com>
2018-02-17 14:22:15 -0600

    changing playgame form

--- client/src/components/PlayGame/PlayGame.js
+++ client/src/components/PlayGame/PlayGame.js
@@ -1,45 +1,73 @@
-import React from "react";
+import React, { Component } from "react";
 import "./PlayGame.css";
 
-const PlayGame = props =>
+class PlayGame extends Component {
+  state = {
 
-<form className="playgame">
-    <div className="form-group">
-      <h2> Join a game</h2>
-      <input
-        type="text"
-        className="form-control"
-        placeholder="GAME CODE"
-        id="joingame-input"
-      />
+  };
 
-      <button
-        type="submit"
-        className="join-btn"
-      >
-        Play!
-      </button>
+  handleInputChange = event => {
+    // Getting the value and name of the input which triggered the change
+    const value = event.target.value;
+    const name = event.target.name;
 
-      <h2> Start a new game </h2>
-      <input
-        type="text"
-        className="form-control"
-        placeholder="# of players"
-        id="startgame-input"
-      />
-      <input
-        type="text"
-        className="form-control"
-        placeholder="password"
-        id="password-input"
-      />
-      <button
-        type="submit"
-        className="start-btn"
-      >
-        Create
-      </button>
-    </div>
-  </form>;
+    // Updating the input's state
+    this.setState({
+      [name]: value
+    });
+  };
+
+  handleFormSubmit = event => {
+
+  }
+
+  render() {
+    return (
+      <form className="playgame">
+        <div className="form-group">
+          <h2> Join a game</h2>
+          <input
+            type="text"
+            className="form-control"
+            onChange={this.handleInputChange}
+            placeholder="GAME CODE"
+            id="joingame-input"
+          />
+
+          <button
+            type="submit"
+            className="join-btn"
+            onClick={this.handleFormSubmit}
+          >
+            Play!
+          </button>
+
+          <h2> Start a new game </h2>
+          <input
+            type="text"
+            className="form-control"
+            onChange={this.handleInputChange}
+            placeholder="# of players"
+            id="startgame-input"
+          />
+          <input
+            type="text"
+            className="form-control"
+            onChange={this.handleInputChange}
+            placeholder="password"
+            id="password-input"
+          />
+          <button
+            type="submit"
+            className="start-btn"
+            onClick={this.handleFormSubmit}
+          >
+            Create
+          </button>
+        </div>
+      </form>
+    )
+  }
+}
 
 export default PlayGame;
