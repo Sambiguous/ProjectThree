da5ce6a8aff3dd4f3aea9a0a7c82ca5ac6a2cff7
Sam <sjdefrank91@gmail.com>
2018-02-18 16:16:09 -0600

    connect to firebase, add login and logout ability

--- client/src/components/SignupForm/SignupForm.js
+++ client/src/components/SignupForm/SignupForm.js
@@ -1,29 +1,77 @@
 import React from "react";
 import "./SignupForm.css";
+import firebase from '../../firebase';
 
-// Using the datalist element we can create autofill suggestions based on the props.breeds array
-const SignupForm = props =>
+//this is a regex function literally copy/pasted from stack overflow useed to 
+//see if a given string is a valid email address. I have no idea how it works
+function validateEmail(email) {
+  var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
 
+
+class SignupForm extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      email: "",
+      username: "",
+      password: ""
+    };
+  };
+
+  handleInputChange = event => {
+    const{name, value} = event.target
+
+    this.setState({[name]: value});
+  };
+
+  handleSignUp = event => {
+    event.preventDefault();
+
+    let email = this.state.email;
+    let username = this.state.username;
+    let password = this.state.password;
+    //--------validations go here--------------
+    //TODO: ensure email is valid email address
+    //pasword is at least 6 characters long
+    //username is not blank
+    //-----------------------------------------
+    
+    const promise = firebase.auth().createUserWithEmailAndPassword(email, password);
+    promise.catch(e => console.log(e.message));
+    promise.then(user =>{
+      user.updateProfile({displayName: username}).then(() =>{
+        firebase.database().ref().child('users').child(username).set(user.email)
+      })
+    });
+  };
+  render = () =>
   <form className="login">
     <div className="form-group">
 
       <input
-        value={props.email}
+        value={this.state.email}
         type="text"
         className="form-control"
         placeholder="EMAIL"
-        id="email"
+        onChange={this.handleInputChange}
+        name="email"
+        id="email_input"
       />
       <datalist id="email">
         {/*props.logins.map(username => <option value={username} key={username} />)*/}
       </datalist>
 
       <input
-        value={props.username}
+        value={this.state.username}
         type="text"
         className="form-control"
         placeholder="USERNAME"
-        id="username"
+        onChange={this.handleInputChange}
+        name="username"
+        id="username_input"
       />
       <datalist id="username">
         {/*props.logins.map(username => <option value={username} key={username} />)*/}
@@ -31,11 +79,13 @@ const SignupForm = props =>
 
 
       <input
-        value={props.password}
+        value={this.state.password}
         type="password"
         className="form-control"
         placeholder="PASSWORD"
-        id="password"
+        onChange={this.handleInputChange}
+        name="password"
+        id="password_input"
       />
       <datalist id="password">
         {/*props.logins.map(password => <option value={password} key={password} />)*/}
@@ -43,11 +93,11 @@ const SignupForm = props =>
 
       <button
         type="submit"
-        onClick={props.handleFormSubmit}
+        onClick={this.handleSignUp}
         className="btn" 
       >SIGN UP
       </button>
     </div>
   </form>;
-
+}
 export default SignupForm;
---
+++ client/src/firebase.js
@@ -0,0 +1,37 @@
+import * as firebase from 'firebase';
+
+var config = {
+    apiKey: "AIzaSyDPwMmbvrCll1xgRRMjMAUFgwUA6_b_c14",
+    authDomain: "on-deck-1595d.firebaseapp.com",
+    databaseURL: "https://on-deck-1595d.firebaseio.com",
+    storageBucket: "on-deck-1595d.appspot.com",
+};
+
+firebase.initializeApp(config);
+
+
+firebase.auth().onAuthStateChanged(user => {
+    if(user){
+        console.log("logged in successfully");
+        console.log(user);
+
+        let id = user.uid;
+        let email = user.email;
+
+        let userRef = firebase.database().ref().child('loggedIn').child(id)
+        userRef.onDisconnect().remove()
+        userRef.set(email, e =>{ 
+            if(e){
+                console.log(e)
+            } else{
+                firebase.database().ref().child('loggedIn').once('value', snap => console.log(snap.val()))
+            }
+            
+        });
+    } else{
+        
+        console.log("user is not logged in");
+    };
+});
+
+export default firebase;
