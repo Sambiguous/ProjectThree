5bcfe7d09a38589dfb90b95c370b097aa3651164
emfritz2 <emfritz2@gmail.com>
2018-02-21 21:28:32 -0600

    removed left and right panels, added for loop to the game.js to start creating individual cards

--- client/src/components/PlayingCards/PlayingCards.js
+++ client/src/components/PlayingCards/PlayingCards.js
@@ -4,14 +4,35 @@ import "./PlayingCards.css";
 class PlayingCards extends Component {
 
 	state = {
+		allCards: {}
+
 
 	}
+		updatePlaying = event => {
+			event.preventDefault();
+
+			//this is where we're pulling hand size in cards
+			// set up an object each card gets sent to
+		}
 
 	render() {
+		const arrayOfStuff = [["A", "Spades"]];
 		return (
 			<div>
 				<div className="playing-cards">
-					<h4> Playing Cards </h4>
+					<div>
+			            {(()=>{
+			              let container =[];
+			              let arr = arrayOfStuff //can be anything array, object 
+			              arr.forEach((val,index)=>{
+			                container.push(<div key={index}>
+			                              <h2>{val[0]}</h2>
+			                              <h3>{val[1]}</h3>
+			                              </div>)
+			                            });
+			              return container;     
+			            })()}
+          			</div>
 				</div>
 			</div>
 		)
--- client/src/pages/Game.js
+++ client/src/pages/Game.js
@@ -2,27 +2,85 @@ import React, { Component } from "react";
 import Container from "../components/Container";
 import Row from "../components/Row";
 import Col from "../components/Col";
-import LeftPanel from "../components/LeftPanel";
-import RightPanel from "../components/RightPanel";
-import "./Game.css";
-
+import DiscardPile from "../components/DiscardPile";
+import CardPile from "../components/CardPile";
+import PlayingCards from "../components/PlayingCards";
+import "./Game.css"; 
 
 class Game extends Component {
+
+	constructor(props) {
+
+		super(props)
+			this.state = {
+			code: "12345",
+			name: "Roulette",
+			players: ["FloridaMan", "Normie", "NutBar", "SamBiguous"],
+			allCards: [  {
+				fromDeck: "USSR jest",
+				fieldInfo: ["No bullet"]
+			  },
+			  {
+				fromDeck: "USSR jest",
+				fieldInfo: ["No bullet"]
+			  },
+			  {
+				fromDeck: "USSR jest",
+				fieldInfo: ["No bullet"]
+			  },
+			  {
+				fromDeck: "USSR jest",
+				fieldInfo: ["No bullet"]
+			  },
+			  {
+				fromDeck: "USSR jest",
+				fieldInfo: ["No bullet"]
+			  },
+			  {
+				fromDeck: "USSR jest",
+				fieldInfo: ["bullet"]
+			  },
+			],
+			discardPile: [{
+				fromDeck: "USSR jest",
+				fieldInfo: ["No bullet"]
+			  },
+			  {
+				fromDeck: "USSR jest",
+				fieldInfo: ["No bullet"]
+			  },
+			  {
+				fromDeck: "USSR jest",
+				fieldInfo: ["No bullet"]
+			  },
+			  {
+				fromDeck: "USSR jest",
+				fieldInfo: ["No bullet"]
+			  },
+			  {
+				fromDeck: "USSR jest",
+				fieldInfo: ["No bullet"]
+			  },
+			  {
+				fromDeck: "USSR jest",
+				fieldInfo: ["bullet"]
+			  },],
+			hand:[]
+		}
+	}
 
 	render() {
 		return (
 			<Container className="card-container">
 				<Row>
-					<Col size="sm-4">
-						<LeftPanel />
-					</Col>
-					<Col size="sm-8">
-						<RightPanel />
-					</Col>
+					<CardPile />
+					<DiscardPile />
+					<PlayingCards />
 				</Row>
 			</Container>		
 		);
 	}
+
 }
 
 export default Game;
