c5b2736d212087cb28a6af2c5364daf85fdec44f
C Ferrell <ferrellchristianr@gmail.com>
2018-03-03 13:38:45 -0600

    api only added

--- routes/api/api.js
+++ routes/api/api.js
@@ -2,6 +2,25 @@ const router = require("express").Router();
 const db = require("../../models")
 const firebase = require("../../firebase");
 
+function shuffle(originalArray) {
+    var array = [].concat(originalArray);
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
 
 router.get("/", function(req, res){
     console.log(req.body);
@@ -62,7 +81,44 @@ router.post('/deckpull', function(req, res){
     db.Deck.findOne({deckName: req.body.name}).populate('allCards').exec(function(err, doc) {
         if (err) throw err
         res.send(doc);
+
     });
 })
+
+router.post('/deckpullnewgame', function(req, res){
+    // console.log("req.body"+req.body.gameName);
+    db.Deck.findOne({deckName: req.body.gameName}).populate('allCards').exec(function(err, doc) {
+        if (err) throw err
+        // res.send(doc);
+        console.log("pull new game deck information:", doc);
+
+        //Check each card to see if null
+        checkNull(doc); 
+
+        // //build object that will be the base state of the game
+        // const newGame = {
+        //     maxPlayers: rec.body.gameNumPlayers,
+        //     GM: rec.body.gameAdmin,
+        //     players: [rec.body.gameAdmin],
+        //     //allCards: allCards, //need to build this with no null fields
+        //     discardPile: ["cards"],
+        //     //cardPile: ["cards"].concat(shuffle(allCards)),
+        //     hands: {}
+        // }
+
+        // newGame.hands[rec.body.gameAdmin] = ['cards']
+        
+        // console.log("new game: "+newGame);
+    });
+})
+
+function checkNull (data) {
+    // // console.log("Check Null Data: " + data);
+    // console.log("data dot allcards: " + data.allCards);
+
+    for (i in data.allCards) {
+        console.log("check null: card#"+i);
+    }
+}
 
 module.exports = router
