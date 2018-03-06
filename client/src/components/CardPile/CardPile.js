c3014d8cecfa16c4be1c953bd60790a4c41f82f2
Rich T <rtranter25@gmail.com>
2018-03-06 14:38:44 -0600

    added popup and trying to make it work

--- client/src/components/CardPile/CardPile.js
+++ client/src/components/CardPile/CardPile.js
@@ -1,18 +1,21 @@
 import React, {Component} from "react";
 import "./CardPile.css";
+import Popup from "../Popup";
 
 class CardPile extends Component {
 
 	constructor(props){
 		super(props);
+    this.togglePopup = this.togglePopup.bind(this);
 		this.state = {
-      cards: props.cardPile
+      cards: props.cardPile,
+      showPopup: false
     }
   }
   
   handleClick = () => {
     if(!this.props.canDeal){return};
-    let deal = prompt("how many?")
+    let deal = prompt("How many cards would you like to deal?")
   	if (deal == null || deal == "") {
         let num = 0;
     } else {
@@ -21,12 +24,27 @@ class CardPile extends Component {
     this.props.deal(deal);
   }
 
+  togglePopup() {
+    this.props.deal(this.props.value);
+    console.log(this.props.value);
+    this.setState({
+      showPopup: !this.state.showPopup
+    });
+  }
+
 	render() {
 		return (
 			<div>
-				<div className="card-pile" onClick={this.handleClick}>
+				<div className="card-pile" onClick={this.togglePopup}>
 					{/*<h5>{this.state.cards.cardPile.length}</h5>*/}
 				</div>
+        {this.state.showPopup ? 
+          <Popup
+            closePopup={this.togglePopup}
+
+          />
+          : null
+        }
 			</div>
 		)
 	}
