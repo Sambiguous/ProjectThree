a4ecf6cd7bedf202209ee2c86d8f0493427ca9e4
C Ferrell <ferrellchristianr@gmail.com>
2018-03-07 20:42:45 -0600

    card BG CLR radio buttons complete

--- client/src/components/BgColor/BgColor.js
+++ client/src/components/BgColor/BgColor.js
@@ -9,7 +9,7 @@ class BgColor extends Component {
   handleInputChange = event => {
     const value = event.target.value;
     const name = event.target.name;
-
+    
     this.props.input(name, value, this.props.index)
   }
 
@@ -18,7 +18,39 @@ class BgColor extends Component {
 
     return (
       <form className="deck-form" name={formName}>
-        <input name={"bgColor"} onChange={this.handleInputChange} type="text" className="deck" placeholder="Card BG Color"/>
+        <h4>Change bg color?</h4>
+        {/* <input name="bgColor" onChange={this.handleInputChange} className="deck" type="text" placeholder="R/B/G/Y"/> */}
+        <div className="radio">
+          <label>
+            <input name="bgColor" type="radio" value="white" onChange={this.handleInputChange} className="deck" checked={true} />
+            White
+          </label>
+        </div>
+        <div className="radio">
+          <label>
+            <input name="bgColor" type="radio" value="red" onChange={this.handleInputChange} className="deck" />
+            Red
+          </label>
+        </div>
+        <div className="radio">
+          <label>
+            <input name="bgColor" type="radio" value="blue" onChange={this.handleInputChange} className="deck" />
+            Blue
+          </label>
+        </div>
+        <br/>
+        <div className="radio">
+          <label>
+            <input name="bgColor" type="radio" value="green" onChange={this.handleInputChange} className="deck" />
+            Green
+          </label>
+        </div>
+        <div className="radio">
+          <label>
+            <input name="bgColor" type="radio" value="yellow" onChange={this.handleInputChange} className="deck" />
+            Yellow
+          </label>
+        </div>
       </form>
     )
   }
