475306c6cb1ad8f715b765f6ec086a438ee0470a
Sam <sjdefrank91@gmail.com>
2018-10-20 15:51:27 -0500

    seedDB finished

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -1,238 +1,249 @@
 const mongoose = require("mongoose");
 const db = require("../models");
+const instance = process.argv[2];
+let proceed;
+let decks = 0;
 mongoose.Promise = global.Promise;
 
-const instance = process.argv[2];
-
 if(instance === "dev"){
+  proceed = true;
   mongoose.connect("mongodb://localhost/onDeck_DB");
   console.log("Seeding development database");
 }else if(instance === "production"){
+  proceed = true;
   mongoose.connect("mongodb://heroku_x6fqxrdj:bhjko6k9c3d5asopm2hdhiq2g6@ds261838.mlab.com:61838/heroku_x6fqxrdj");
   console.log("Seeding Production Database (I hope that's what you meant to do)");
+}else{
+  proceed = false;
+  console.log("you must enter either 'dev' or 'production' after the filename to designate which DB you want to seed");
 }
 
-
-const userSeed = [
-  {
-    username: 'OnDeck',
-    password: '12345',
-    createdDecks: []
-  }
-]
-
-const deckSeed = [
-  {
-    deckName: "USSR jest",
-    numCards: 6,
-    numFields: 1,
-    createBy: "onDeck",
-    handSize: 1,
-    allCards: []
-  },
-  {
-    deckName: "Classic Deck",
-    numCards: 52,
-    numFields: 2,
-    createdBy: "onDeck",
-    handSize: 5,
-    allCards: []
-  },
-  {
-    deckName: "Uno",
-    numCards: 108,
-    numFields: 1,
-    createdBy: "onDeck",
-    handSize: 7,
-    allCards: []
-  }
- ];
-
-let cardSeed = [
-  {
-    fromDeck: "USSR jest",
-    "fieldInfo0": "Loaded barrel",
-    "fieldInfo1": null,
-    "fieldInfo2": null,
-    "fieldInfo3": null,
-    "fieldInfo4": null,
-    "fieldInfo5": null
-  },
-  {
-    fromDeck: "USSR jest",
-    "fieldInfo0": "Loaded barrel",
-    "fieldInfo1": null,
-    "fieldInfo2": null,
-    "fieldInfo3": null,
-    "fieldInfo4": null,
-    "fieldInfo5": null
-  },
-  {
-    fromDeck: "USSR jest",
-    "fieldInfo0": "Loaded barrel",
-    "fieldInfo1": null,
-    "fieldInfo2": null,
-    "fieldInfo3": null,
-    "fieldInfo4": null,
-    "fieldInfo5": null
-  },
-  {
-    fromDeck: "USSR jest",
-    "fieldInfo0": "Loaded barrel",
-    "fieldInfo1": null,
-    "fieldInfo2": null,
-    "fieldInfo3": null,
-    "fieldInfo4": null,
-    "fieldInfo5": null
-  },
-  {
-    fromDeck: "USSR jest",
-    "fieldInfo0": "Loaded barrel",
-    "fieldInfo1": null,
-    "fieldInfo2": null,
-    "fieldInfo3": null,
-    "fieldInfo4": null,
-    "fieldInfo5": null
-  },
-  {
-    fromDeck: "USSR jest",
-    "fieldInfo0": "NOT Loaded barrel",
-    "fieldInfo1": null,
-    "fieldInfo2": null,
-    "fieldInfo3": null,
-    "fieldInfo4": null,
-    "fieldInfo5": null
-  }
-];
-
-const makeClassicDeck = () => {
-  const suits = ['Diamonds', 'Hearts', 'Spades', 'Clubs'];
-  const values = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
-  for(var i=0; i < suits.length; i++){
-    for(var k=0; k < values.length; k++){
-      cardSeed.push({
-        fromDeck: "Classic Deck",
-        "fieldInfo0": values[k],
-        "fieldInfo1": suits[i],
-        "fieldInfo2": null,
-        "fieldInfo3": null,
-        "fieldInfo4": null,
-        "fieldInfo5": null,
-        "bgColor": "white"
-      })
+if(proceed){
+  const userSeed = [
+    {
+      username: 'OnDeck',
+      password: '12345',
+      createdDecks: []
+    }
+  ]
+  
+  const deckSeed = [
+    {
+      deckName: "USSR jest",
+      numCards: 6,
+      numFields: 1,
+      createBy: "onDeck",
+      handSize: 1,
+      allCards: []
+    },
+    {
+      deckName: "Classic Deck",
+      numCards: 52,
+      numFields: 2,
+      createdBy: "onDeck",
+      handSize: 5,
+      allCards: []
+    },
+    {
+      deckName: "Uno",
+      numCards: 108,
+      numFields: 1,
+      createdBy: "onDeck",
+      handSize: 7,
+      allCards: []
+    }
+   ];
+  
+  let cardSeed = [
+    {
+      fromDeck: "USSR jest",
+      "fieldInfo0": "Loaded barrel",
+      "fieldInfo1": null,
+      "fieldInfo2": null,
+      "fieldInfo3": null,
+      "fieldInfo4": null,
+      "fieldInfo5": null
+    },
+    {
+      fromDeck: "USSR jest",
+      "fieldInfo0": "Loaded barrel",
+      "fieldInfo1": null,
+      "fieldInfo2": null,
+      "fieldInfo3": null,
+      "fieldInfo4": null,
+      "fieldInfo5": null
+    },
+    {
+      fromDeck: "USSR jest",
+      "fieldInfo0": "Loaded barrel",
+      "fieldInfo1": null,
+      "fieldInfo2": null,
+      "fieldInfo3": null,
+      "fieldInfo4": null,
+      "fieldInfo5": null
+    },
+    {
+      fromDeck: "USSR jest",
+      "fieldInfo0": "Loaded barrel",
+      "fieldInfo1": null,
+      "fieldInfo2": null,
+      "fieldInfo3": null,
+      "fieldInfo4": null,
+      "fieldInfo5": null
+    },
+    {
+      fromDeck: "USSR jest",
+      "fieldInfo0": "Loaded barrel",
+      "fieldInfo1": null,
+      "fieldInfo2": null,
+      "fieldInfo3": null,
+      "fieldInfo4": null,
+      "fieldInfo5": null
+    },
+    {
+      fromDeck: "USSR jest",
+      "fieldInfo0": "NOT Loaded barrel",
+      "fieldInfo1": null,
+      "fieldInfo2": null,
+      "fieldInfo3": null,
+      "fieldInfo4": null,
+      "fieldInfo5": null
+    }
+  ];
+  
+  const makeClassicDeck = () => {
+    const suits = ['Diamonds', 'Hearts', 'Spades', 'Clubs'];
+    const values = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+    for(var i=0; i < suits.length; i++){
+      for(var k=0; k < values.length; k++){
+        cardSeed.push({
+          fromDeck: "Classic Deck",
+          "fieldInfo0": values[k],
+          "fieldInfo1": suits[i],
+          "fieldInfo2": null,
+          "fieldInfo3": null,
+          "fieldInfo4": null,
+          "fieldInfo5": null,
+          "bgColor": "white"
+        })
+      };
     };
   };
-};
-
-const makeUnoDeck = () => {
-  const values = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'Skip', 'Reverse', 'Draw Two'];
-  const colors = ['red', 'blue', 'green', 'yellow'];
-
-  for(var i=0; i < colors.length; i++){
-    for(var k=0; k < values.length; k++){
-      let card = {
+  
+  const makeUnoDeck = () => {
+    const values = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'Skip', 'Reverse', 'Draw Two'];
+    const colors = ['red', 'blue', 'green', 'yellow'];
+  
+    for(var i=0; i < colors.length; i++){
+      for(var k=0; k < values.length; k++){
+        let card = {
+          fromDeck: "Uno",
+          "fieldInfo0": values[k],
+          "fieldInfo1": null,
+          "fieldInfo2": null,
+          "fieldInfo3": null,
+          "fieldInfo4": null,
+          "fieldInfo5": null,
+          "bgColor": colors[i]
+        };
+        if(!(values[k] === '0')){
+          cardSeed.push(card);
+        }
+        cardSeed.push(card);
+      }
+      let wild = {        
         fromDeck: "Uno",
-        "fieldInfo0": values[k],
+        "fieldInfo0": "wild",
         "fieldInfo1": null,
         "fieldInfo2": null,
         "fieldInfo3": null,
         "fieldInfo4": null,
         "fieldInfo5": null,
-        "bgColor": colors[i]
+        "bgColor": "white"
+      }
+  
+      let draw4 = {
+        fromDeck: "Uno",
+        "fieldInfo0": "Draw Four",
+        "fieldInfo1": null,
+        "fieldInfo2": null,
+        "fieldInfo3": null,
+        "fieldInfo4": null,
+        "fieldInfo5": null,
+        "bgColor": "white"
       };
-      if(!(values[k] === '0')){
-        cardSeed.push(card);
-      }
-      cardSeed.push(card);
-    }
-    let wild = {        
-      fromDeck: "Uno",
-      "fieldInfo0": "wild",
-      "fieldInfo1": null,
-      "fieldInfo2": null,
-      "fieldInfo3": null,
-      "fieldInfo4": null,
-      "fieldInfo5": null,
-      "bgColor": "white"
-    }
-
-    let draw4 = {
-      fromDeck: "Uno",
-      "fieldInfo0": "Draw Four",
-      "fieldInfo1": null,
-      "fieldInfo2": null,
-      "fieldInfo3": null,
-      "fieldInfo4": null,
-      "fieldInfo5": null,
-      "bgColor": "white"
+  
+      cardSeed.push(wild);
+      cardSeed.push(draw4);
     };
-
-    cardSeed.push(wild);
-    cardSeed.push(draw4);
   };
-};
-
-makeUnoDeck();
-makeClassicDeck();
-
-db.Deck
-
-  //delete deck collection
-  .remove({})
-
-  //then delete cards collection
-  .then(db.Card.remove({})
-
-  //then delete the User collection
-  .then(db.User.remove({})
-
-  //then insert the seed user
-  .then(() => db.User.collection.insertOne(userSeed[0]))
-
-  //then insert the seed deck
-  .then(userData => db.Deck.collection.insertMany(deckSeed)
-
-  //then...
-  .then(deckData => {
-
-    //create array of ids of inserted decks
-    let deckIds = Object.keys(deckData.insertedIds).map(key => deckData.insertedIds[key])
-
-    //update the seed user so that the inserted deck belongs to him
-    db.User.findOneAndUpdate({_id: userData.insertedId}, {$set:{createdDecks: deckIds} }, {new: true}, function(err, doc){
-      if(err) throw err
-    });
-
-    //fill cards array with instances of db.Card based on the info in cardSeed
-    let cards = cardSeed.map(card => new db.Card(card));
-    
-    //insert all the cards into the cards collection
-    db.Card.insertMany(cards, function(err, insertedDocs){
-      console.log(deckData);
-      if(err) throw err;
-     
-      //seperate _ids of cards into arrays based on the fromDeck property and put them in an object
-      let ids = {
-        "Uno": insertedDocs.filter(element => element.fromDeck === "Uno").map(element => element._id),
-        "Classic Deck": insertedDocs.filter(element => element.fromDeck === "Classic Deck").map(element => element._id),
-        "USSR jest": insertedDocs.filter(element => element.fromDeck === "USSR jest").map(element => element._id)
-      }
-
-      //find the deck that the cards belong to and set the deck's allCards property to the ids array
-      for(var i=0; i < deckData.ops.length; i++){
-        let name = deckData.ops[i].deckName
-
-        db.Deck.findOneAndUpdate({deckName: name}, {$set:{ allCards: ids[name] }}, {new: true}, function(err, doc){
-
-          if (err) {
-            console.log(err);
-
-          } else {
-            
-            console.log("all good yo");
-          };
-        });
-      }
-    });
-  })
-)));
+  
+  makeUnoDeck();
+  makeClassicDeck();
+  
+  db.Deck
+  
+    //delete deck collection
+    .remove({})
+  
+    //then delete cards collection
+    .then(db.Card.remove({})
+  
+    //then delete the User collection
+    .then(db.User.remove({})
+  
+    //then insert the seed user
+    .then(() => db.User.collection.insertOne(userSeed[0]))
+  
+    //then insert the seed deck
+    .then(userData => db.Deck.collection.insertMany(deckSeed)
+  
+    //then...
+    .then(deckData => {
+  
+      //create array of ids of inserted decks
+      let deckIds = Object.keys(deckData.insertedIds).map(key => deckData.insertedIds[key])
+  
+      //update the seed user so that the inserted deck belongs to him
+      db.User.findOneAndUpdate({_id: userData.insertedId}, {$set:{createdDecks: deckIds} }, {new: true}, function(err, doc){
+        if(err) throw err
+      });
+  
+      //fill cards array with instances of db.Card based on the info in cardSeed
+      let cards = cardSeed.map(card => new db.Card(card));
+      
+      //insert all the cards into the cards collection
+      db.Card.insertMany(cards, function(err, insertedDocs){
+        if(err) throw err;
+       
+        //seperate _ids of cards into arrays based on the fromDeck property and put them in an object
+        let ids = {
+          "Uno": insertedDocs.filter(element => element.fromDeck === "Uno").map(element => element._id),
+          "Classic Deck": insertedDocs.filter(element => element.fromDeck === "Classic Deck").map(element => element._id),
+          "USSR jest": insertedDocs.filter(element => element.fromDeck === "USSR jest").map(element => element._id)
+        }
+  
+        //find the deck that the cards belong to and set the deck's allCards property to the ids array
+        for(var i=0; i < deckData.ops.length; i++){
+          let name = deckData.ops[i].deckName
+  
+          db.Deck.findOneAndUpdate({deckName: name}, {$set:{ allCards: ids[name] }}, {new: true}, function(err, doc){
+  
+            if (err) {
+              console.log(err);
+  
+            } else {
+              
+              console.log(`${name} seeded successfully`);
+              decks++
+              if(decks === deckData.ops.length){
+                console.log("closing connection")
+                mongoose.connection.close();
+              }
+            };
+          });
+        }
+      });
+    })
+  )));
+}
